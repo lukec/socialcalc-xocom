9656ed3f3945275792da74afe3f4dc394c8576a0
Luke Closs <olpc@5thplane.com>
2008-04-26 19:27:42 -0400

    build an activity file

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python
+
+# Make the MANIFEST file.  The contents of the activity folder and the NEWS and MANIFEST
+# files are already included in the .xo file which the bundlebuilder creates, so don't
+# add them to the manifest file.
+import os
+os.system("find ./ -name 'activity' -prune -o -name 'NEWS' \
+                                    -prune -o -name 'MANIFEST' \
+                                    -prune -o -name '.svn' \
+                                    -prune -o -name '*.pyc' \
+                                    -prune -o -name '*.xo' \
+                                    -prune -o -type f \
+                   -print > MANIFEST") 
+
 from sugar.activity import bundlebuilder
-bundlebuilder.start("OnePageWikiActivity")
+bundlebuilder.start("SocialCalcActivity")
