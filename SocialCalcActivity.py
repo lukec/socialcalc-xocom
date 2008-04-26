f25b1049b7dc1fb191329355ca1f3e2e693133ff
Luke Closs <olpc@5thplane.com>
2008-04-26 19:40:24 -0400

    update the base activity from a recent OnePageWiki example

--- SocialCalcActivity.py
+++ SocialCalcActivity.py
@@ -1,34 +1,29 @@
 from sugar.activity import activity
-from sugar.activity.activity import get_bundle_path
 from sugar import env
-import logging
-import sys, os
+import os
 import gtk
 import gobject
-
 import hulahop
 hulahop.startup(os.path.join(env.get_profile_path(), 'gecko'))
-
 from XOCom import XOCom
 
-# The XOCom object helps us communicate with the browser
-uri = 'file://' + get_bundle_path() + '/web/index.html';
-xocom = XOCom(uri)
-
-class OnePageWikiActivity (activity.Activity):
+class SocialCalcActivity (activity.Activity):
     def __init__(self, handle):
         activity.Activity.__init__(self, handle)
-        self.set_title('OnePageWiki')
+        self.set_title('SocialCalc')
+
+        # The XOCom object helps us communicate with the browser
+        # This uses web/index.html as the default page to load
+        self.xocom = XOCom()
 
         toolbox = activity.ActivityToolbox(self)
         self.set_toolbox(toolbox)
         toolbox.show()
 
-        self.set_canvas( xocom.create_webview() )
+        self.set_canvas( self.xocom.create_webview() )
 
     def write_file(self, filename):
-        content = xocom.send_to_browser('write')
-        print "write_file(%s): %s"%(filename, content)
+        content = self.xocom.send_to_browser('write')
         if content:
             fh = open(filename, 'w')
             fh.write(content)
@@ -37,9 +32,8 @@ class OnePageWikiActivity (activity.Activity):
     def read_file(self, filename):
         fh = open(filename, 'r')
         content = fh.read()
-        print "read_file(%s): %s"%(filename, content)
         # We must delay this to give the browser time to start up
         # It would be better if this send_to_browser was instead triggered
         # once the browser had finished loading.
-        gobject.timeout_add(2000, xocom.send_to_browser, 'read', content)
+        gobject.timeout_add(2000, self.xocom.send_to_browser, 'read', content)
         
