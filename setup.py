2ee1c497ad96f4e76f0537ec7f4d36281cffaa17
Luke Closs <olpc@5thplane.com>
2008-12-15 04:01:28 +0000

    update to use the latest packaging code, produce a new .xo

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@
 # Make the MANIFEST file.  The contents of the activity folder and the NEWS and MANIFEST
 # files are already included in the .xo file which the bundlebuilder creates, so don't
 # add them to the manifest file.
-import os
-os.system("find ./ -name 'activity' -prune -o -name 'NEWS' \
-                                    -prune -o -name 'MANIFEST' \
-                                    -prune -o -name '.svn' \
-                                    -prune -o -name '*.pyc' \
-                                    -prune -o -name '*.xo' \
-                                    -prune -o -name '*.swp' \
-                                    -prune -o -name '.git*' \
-                                    -prune -o -type f \
-                   -print > MANIFEST") 
 
-from sugar.activity import bundlebuilder
-bundlebuilder.start("SocialCalcActivity")
+try:
+    from sugar.activity import bundlebuilder
+    bundlebuilder.start()
+except ImportError:
+    import os
+    os.system("find ./ | sed 's,^./,SocialCalcActivity.activity/,g' > MANIFEST")
+    os.chdir('..')
+    os.system('zip -r SocialCalcActivity.xo SocialCalcActivity.activity')
+    os.system('mv SocialCalcActivity.xo ./SocialCalcActivity.activity')
+    os.chdir('SocialCalcActivity.activity')
+
